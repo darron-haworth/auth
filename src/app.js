db13309c180b5ae883db8c217181c7439d5038ed
Darron Haworth <darron.haworth@firsttechfed.com>
2016-11-11 15:10:16 -0800

    pulled in firebase: /learn/v4/t/lecture/5744126

--- src/app.js
+++ src/app.js
@@ -1,8 +1,20 @@
 import React, { Component } from 'react';
 import { View, Text } from 'react-native';
+import firebase from 'firebase';
 import { Header } from './components/common';
 
+
+
 class App extends Component {
+    componentWillMount() {
+        firebase.initializeApp({
+            apiKey: 'AIzaSyDYvfp4yCNxiyF-39mPrAC0fqdRkOTaLvU',
+            authDomain: 'udemy-rn-auth.firebaseapp.com',
+            databaseURL: 'https://udemy-rn-auth.firebaseio.com',
+            storageBucket: 'udemy-rn-auth.appspot.com',
+            messagingSenderId: '454126433225'
+        });
+    }
     render() {
         return (
             <View>
