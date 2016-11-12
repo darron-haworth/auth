4f97dd5dfb5d0e9fb41ec80d017ae463e0b07915
Darron Haworth <darron.haworth@firsttechfed.com>
2016-11-12 10:37:53 -0800

    login check: /learn/v4/t/lecture/5744178

--- src/app.js
+++ src/app.js
@@ -1,12 +1,12 @@
 import React, { Component } from 'react';
 import { View } from 'react-native';
 import firebase from 'firebase';
-import { Header } from './components/common';
+import { Header, Button, Spinner } from './components/common';
 import LoginForm from './components/LoginForm';
 
 
 class App extends Component {
-    state = { loggedIn: false }
+    state = { loggedIn: null }
     componentWillMount() {
         firebase.initializeApp({
             apiKey: 'AIzaSyDYvfp4yCNxiyF-39mPrAC0fqdRkOTaLvU',
@@ -24,11 +24,24 @@ class App extends Component {
             }
         });
     }
+
+    renderContent() {
+        switch (this.state.loggedIn) {
+            case true:
+            return (<Button>Log Out!</Button>);
+            case false:
+            return (<LoginForm />);
+            default:
+                return (<View style={{ flex: 1, alignItems: 'center', alignSelf: 'center' }}><Spinner style={{alignItems: 'center', alignSelf: 'center'}} /></View>);
+        }
+    }
+        
+    
     render() {
         return (
             <View>
                 <Header headerText='Authentication' />
-                <LoginForm />
+                {this.renderContent()}
             </View>
         );
     }
